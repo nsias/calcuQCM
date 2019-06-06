c6ec6d036b168947cc6dd94fa09e98c9416b83f0
Maxime Beugoms <maxime.beugoms@student.uclouvain.be>
2019-06-06 15:47:38 +0200

    fast fix for infinity boucle ch5 demo

--- data/theorems/ch5.js
+++ data/theorems/ch5.js
@@ -1,21 +1,21 @@
 var theoremCh5=[
   {
-    "name" : "Propriété de formalisme de calculabilité",
+    "name" : "Propriété de formalisme de calculabilité 1",
     "enonce": "ch5/enonce_00",
     "solution": "ch5/solution_00"
   },
   {
-    "name" : "Propriété de formalisme de calculabilité",
+    "name" : "Propriété de formalisme de calculabilité 2",
     "enonce": "ch5/enonce_01",
     "solution": "ch5/solution_01"
   },
   {
-    "name" : "Propriété de formalisme de calculabilité",
+    "name" : "Propriété de formalisme de calculabilité 3",
     "enonce": "ch5/enonce_02",
     "solution": "ch5/solution_02"
   },
   {
-    "name" : "Propriété de formalisme de calculabilité",
+    "name" : "Propriété de formalisme de calculabilité 4",
     "enonce": "ch5/enonce_03",
     "solution": "ch5/solution_03"
   }
